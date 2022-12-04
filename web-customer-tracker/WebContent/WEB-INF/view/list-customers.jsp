ef80526ac3954a452578a6610b980691b5b26751
Dino Babić <dinobabi3@gmail.com>
2022-12-04 21:28:43 +0100

    Adding CRUD operations

--- web-customer-tracker/WebContent/WEB-INF/view/list-customers.jsp
+++ web-customer-tracker/WebContent/WEB-INF/view/list-customers.jsp
@@ -1,12 +1,69 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
 <!DOCTYPE html>
 <html>
-<head>
-<meta charset="ISO-8859-1">
-	<title>Customers</title>
-</head>
-<body>
-	<h1>Customer's list</h1>
-</body>
+	<head>
+		<meta charset="ISO-8859-1">
+		<title>List Customers</title>
+		<link type="text/css" rel="stylesheet" 
+			href="${pageContext.request.contextPath}/resources/css/style.css"/>
+	</head>
+	<body>
+		<div class="wrapper">
+			<div id="header">
+				<h2>CRM - Customer Relationship Manager</h2>
+			</div>
+		</div>
+		
+		<div class="container">
+			<div class="context">
+				
+				<input type="button" value="Add Customer" 
+					onclick="window.location.href='showFormForAdd'; return false;"
+					class="add-button"
+				/>
+				
+				<form:form action="search" method="GET">
+					
+					Search Customer: <input type="text" name="searchName"/>
+					<input type="submit" value="Search" class="add-button"/>
+				
+				</form:form>
+			
+				<table>
+					<tr>
+						<th>First Name</th>
+						<th>Last Name</th>
+						<th>Email</th>
+						<th>Action</th>
+					</tr>
+					
+					<c:forEach var="tempCustomer" items="${customers}">
+						<c:url var="updateLink" value="/customer/showFormForUpdate">
+							<c:param name="customerId" value="${tempCustomer.id}"/>
+						</c:url>
+						
+						<c:url var="deleteLink" value="/customer/delete">
+							<c:param name="customerId" value="${tempCustomer.id}"/>
+						</c:url>
+						
+						<tr>
+							<td> ${tempCustomer.firstName} </td>
+							<td> ${tempCustomer.lastName}</td>
+							<td> ${tempCustomer.email}</td>
+							<td>
+								<a href="${updateLink}">Update</a>
+								|
+								<a href ="${deleteLink}"
+								onclick="if (!(confirm('Are you sure you want to delete this customer?'))) return false;">Delete</a>
+							</td>
+						</tr>
+					</c:forEach>
+				</table>
+			</div>
+		</div>
+		
+	</body>
 </html>
---
+++ web-customer-tracker/src/com/springdemo/dao/CustomerDAOImpl.java
@@ -0,0 +1,90 @@
+package com.springdemo.dao;
+
+import java.util.List;
+
+import org.hibernate.query.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import com.springdemo.entity.Customer;
+
+@Repository
+public class CustomerDAOImpl implements CustomerDAO {
+	
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	@Override
+	public List<Customer> getCustomers() {
+		
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		Query<Customer> query = currentSession.createQuery("from Customer order by lastName", Customer.class);
+		
+		List<Customer> customers = query.getResultList();
+		
+		return customers;
+	}
+
+	@Override
+	public void saveCustomer(Customer customer) {
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		currentSession.saveOrUpdate(customer);
+	}
+
+	@Override
+	public Customer getCustomer(int id) {
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		Customer customer = currentSession.get(Customer.class, id);
+		
+		return customer;
+	}
+
+	@Override
+	public void deleteCustomer(int id) {
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		Query query = currentSession.createQuery("delete from Customer where id=:customerId");
+		query.setParameter("customerId", id);
+		
+		query.executeUpdate();
+	}
+
+	@Override
+	public List<Customer> searchCustomers(String searchName) {
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		Query query = null;
+		
+		if (searchName != null && searchName.trim().length() > 0) {
+			query = currentSession.createQuery("from Customer where "
+					+ "lower(firstName) like :name or lower(lastName) "
+					+ "like :name", Customer.class);
+			query.setParameter("name", "%" + searchName.toLowerCase() + "%");
+		}
+		else {
+			query = currentSession.createQuery("from Customer", Customer.class);
+		}
+		
+		List<Customer> customers = query.getResultList();
+		
+		return customers;
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
